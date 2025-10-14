071c85e325042070b8d0249eadc360140746c148
Siphalor <info@siphalor.de>
2025-10-14 19:59:28 +0200

    fix: Polish the config a bit

--- src/config.php
+++ src/config.php
@@ -10,12 +10,12 @@ define("PRIMARY_COLOR", $_ENV['MAVENRV_PRIMARY_COLOR'] ?? "#516d29");
 define("PRIMARY_COLOR_DARK_MODE", $_ENV['MAVENRV_PRIMARY_COLOR_DARK_MODE'] ?? $_ENV['MAVENRV_PRIMARY_COLOR'] ?? "#82b342");
 
 # List display
-if (isset($_ENV['MAVENRV_ROOT_DIR'])) {
-    define('IGNORED_EXTENSIONS', explode(',', $_ENV['MAVENRV_ROOT_DIR']));
+if (isset($_ENV['MAVENRV_IGNORED_EXTENSIONS'])) {
+    define('IGNORED_EXTENSIONS', explode(',', $_ENV['MAVENRV_IGNORED_EXTENSIONS']));
 } else {
     define('IGNORED_EXTENSIONS', array('.php', '.md'));
 }
-define('VERSIONS_SORT_BY', $_ENV['MAVENRV_VERSIONS_SORT_BY'] ?? 'last_modified'); # last_modified, name
+define('VERSIONS_SORT_BY', $_ENV['MAVENRV_VERSIONS_SORT_BY'] ?? 'name'); # last_modified, name
 define('VERSIONS_SORT_ORDER', $_ENV['MAVENRV_VERSIONS_SORT_ORDER'] ?? 'desc'); # desc, asc
 
 # Assets
